c9fe69c3944a8913f621201a8fcc72b455b07d3c
Leonel Matos <leonel.amatos@gmail.com>
2022-02-06 23:50:25 -0500

    ADDS - Add the error message to the contact-us input field.

--- bookmark_landing_page/index.js
+++ bookmark_landing_page/index.js
@@ -1,8 +1,23 @@
+// const Joi = require('hapi-joi');
+
 const navItems = document.querySelectorAll('.nav__item.feat');
 const tabContent = document.querySelectorAll('.sections__tab--content');
 const activeIndicator = document.getElementById('indicator');
 const summaryArray = document.querySelectorAll('.detail-items details');
-const arrows = document.querySelectorAll('.arrow');
+const form = document.getElementById('form');
+const emailInput = document.getElementById('email-input');
+const emailInputError = document.getElementById('email-error-message');
+
+form.onsubmit = handleOnSubmit;
+
+const schema = joi.object().keys({
+	email: joi
+		.string()
+		.email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+		.message("Whooops, make sure it's an email"),
+
+	isEmailValid: joi.when('email', { is: joi.valid(true), then: joi.boolean().valid(true) }),
+});
 
 navItems.forEach((item, indx) => {
 	item.addEventListener('click', function () {
@@ -28,24 +43,34 @@ function removeClass() {
 	});
 }
 
-function changeOpenArrow() {
-	// arrows.forEach((arrow) => arrow.for);
-}
-
 summaryArray.forEach((title) => {
-	console.log(title);
 	title.addEventListener('toggle', function () {
 		const svg = this.nextElementSibling.querySelector('.arrow-path');
 		if (this.open) {
 			svg.style.stroke = 'hsl(0, 94%, 66%)';
-			console.log(svg);
-			// console.log(changeOpenArrow());
 		} else {
 			svg.style.stroke = '#5267DF';
-			console.log('close');
 		}
-		// console.log(this.class == 'polyfill open');
 	});
 });
 
-console.log('loaded');
+function handleOnSubmit(event) {
+	event.preventDefault();
+	const errorMessage = schema.validate({
+		email: emailInput.value,
+	});
+
+	const isInvalid = errorMessage.hasOwnProperty('error');
+
+	if (isInvalid) {
+		emailInputError.innerText = errorMessage.error.message;
+
+		emailInput.classList.add('error');
+		emailInputError.classList.add('error-message');
+	} else {
+		emailInputError.innerText = '';
+
+		emailInput.classList.remove('error');
+		emailInputError.classList.remove('error-message');
+	}
+}
