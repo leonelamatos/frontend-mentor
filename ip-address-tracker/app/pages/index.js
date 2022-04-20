815e93aaa80b9dc03562642fec9615445cfbd8ad
leonel a matos <leonel.amatos@gmail.com>
2022-04-20 12:18:31 -0400

    refactor: remove link tag from header tag

--- ip-address-tracker/app/pages/index.js
+++ ip-address-tracker/app/pages/index.js
@@ -1,22 +1,16 @@
 import Head from 'next/head';
+import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import DisplayData from '../Components/DisplayData';
 import Header from '../Components/Header';
-import Map from '../Components/Map';
+// import Map from '../Components/Map';
+const Map = dynamic(() => import('../Components/Map'), { ssr: false });
 
 export default function Home() {
 	return (
 		<>
 			<Head>
 				<title>IP Address Tracker</title>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap'
-					rel='stylesheet'
-				></link>
-				<link
-					href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'
-					rel='stylesheet'
-				></link>
 			</Head>
 			<Header />
 			<DisplayData />
