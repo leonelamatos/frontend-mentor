069ded67beccaff9a5d098def233363819522958
leonel a matos <leonel.amatos@gmail.com>
2022-04-20 12:16:17 -0400

    style: final style of Map component

--- ip-address-tracker/app/Components/Map.jsx
+++ ip-address-tracker/app/Components/Map.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import styles from '../styles/Map.module.css';
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import { useState } from 'react';
+function MapConponent() {
+	const [location, setLocation] = useState({ lat: 51.505, lon: -0.09 });
 
-function Map() {
-	return <div>Map</div>;
+	return (
+		<section className='container'>
+			<MapContainer
+				className={styles.wrapper}
+				center={[location.lat, location.lon]}
+				zoom={16}
+				scrollWheelZoom={true}
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap2</a> contributors'
+					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+				/>
+				<Marker position={[location.lat, location.lon]}>
+					<Popup>
+						A pretty CSS3 popup. <br /> Easily customizable.
+					</Popup>
+				</Marker>
+			</MapContainer>
+		</section>
+	);
 }
 
-export default Map;
+export default MapConponent;
