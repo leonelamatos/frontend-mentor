6d1524985967b4da67eee94bf26e0a668649b7bd
leonel a matos <leonel.amatos@gmail.com>
2022-04-28 15:33:45 -0400

    refactor: cleanup code and  replace api url

--- ip-address-tracker/app/pages/api/dns.js
+++ ip-address-tracker/app/pages/api/dns.js
@@ -1,28 +1,18 @@
 var dns = require('dns').promises;
 const axios = require('axios').default;
 
-const API_KEY = process.env.API_KEY;
 
 export default async function dnslookup(req, res) {
-	const ipAddress = req.body.ip || req.query.ip;
+	const domain = req.body.ip || req.query.ip;
 	try {
-		const { address } = await dns.lookup(ipAddress);
-		console.log(address);
+		const { address } = await dns.lookup(domain);
 
-		const urlString =
-			address === null
-				? `https://api.ipgeolocation.io/ipgeo?apiKey=${API_KEY}`
-				: `https://api.ipgeolocation.io/ipgeo?apiKey=${API_KEY}&ip=${address}`;
+		const result = await axios.get(`https://ipwho.is/${address}`);
 
-		const result = await axios.get(urlString);
+		res.json( { data: result.data } );
 
-		console.log(result.data);
-		res.json(result.data);
 	} catch (error) {
 		console.error(error);
+		res.json({ ...error });
 	}
 }
-
-// address === null
-// 	? `https://ipgeolocation.abstractapi.com/v1/?api_key=${API_KEY}`
-// 	: `https://ipgeolocation.abstractapi.com/v1/?api_key=${API_KEY}&ip_address=${address}`;
