5835b99b278f212d57707f5d48d248fdefabff1f
leonel a matos <leonel.amatos@gmail.com>
2022-04-29 08:55:38 -0400

    refactor: using ip resolver instead of lookup as return more ips

--- ip-address-tracker/app/pages/api/dns.js
+++ ip-address-tracker/app/pages/api/dns.js
@@ -1,12 +1,23 @@
 var dns = require('dns').promises;
 const axios = require('axios').default;
 
+const randomNumber = (max) => Math.floor(Math.random() * max);
+
 export default async function dnslookup(req, res) {
 	const domain = req.body.ip || req.query.ip;
 	try {
-		const { address } = await dns.lookup(domain);
+		// resolve the domain IPv4 if a domain name is sent.
+		// const { address } = await dns.lookup(domain);
+		const address = await dns.resolve(domain);
 
-		const result = await axios.get(`https://ipwho.is/${address}`);
+		// get a random number from the return array length
+		const randomIP = randomNumber(address.length);
+
+		// select an IP address based on the random number
+		const ipAddress = address[randomIP];
+
+		// call the api with the selected ip
+		const result = await axios.get(`https://ipwho.is/${ipAddress}`);
 
 		res.json(result.data);
 	} catch (error) {
